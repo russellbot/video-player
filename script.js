cfd44b4158ff57a52d1cbe473b9cfef9fdaf75a6
Russell Hart <russellbot@gmail.com>
2020-10-20 21:59:37 -0400

    volume mute and unmute

--- script.js
+++ script.js
@@ -56,6 +56,8 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
+
 // Volume Bar
 function changeVolume(e) {
     let volume = e.offsetX / volumeRange.offsetWidth;
@@ -68,7 +70,6 @@ function changeVolume(e) {
     }
     volumeBar.style.width = `${volume * 100}%`;
     video.volume = volume;
-    console.log(volume);
     // Change Icon depending on volume
     volumeIcon.className = '';
     if (volume > 0.7) {
@@ -77,6 +78,24 @@ function changeVolume(e) {
         volumeIcon.classList.add('fas', 'fa-volume-down');
     } else if (volume === 0) {
         volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+    lastVolume = volume;
+}
+
+// Mute/Unmute
+function toggleMute() {
+    volumeIcon.className = '';
+    if (video.volume) {
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add('fas', 'fa-volume-mute');
+        volumeIcon.setAttribute('title', 'Unmute');
+    } else {
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+        volumeIcon.setAttribute('title', 'Mute');
     }
 }
 
@@ -93,3 +112,4 @@ video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
